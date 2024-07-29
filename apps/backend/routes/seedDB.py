76123ff252a2d169c679c9a77f68d15c4a867c02
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-07-29 19:33:17 -0400

    add more to the seed route

--- apps/backend/routes/seedDB.py
+++ apps/backend/routes/seedDB.py
@@ -1,4 +1,4 @@
-from flask import Blueprint 
+from flask import Blueprint, jsonify
 import numpy as np 
 
 seedDatabase = Blueprint('seedDB', __name__)
@@ -11,5 +11,38 @@ def parseFile():
     '''
     with open('./data/allPitches2024.csv') as f: 
         allPitches = [i for i in f.readlines() if i!=',']
+
+    allPitches = np.array(allPitches)
+    allPitches = allPitches[1:]
+
+    #now we need to split into individual lines 
+
+    splitArray = []
+    rowLen = len(allPitches[1])
+    for pitch in allPitches:
+        thisRow = np.zeros(rowLen)
+        thisRow = pitch.split(',')
+          #last 
+        splitArray.append(thisRow[1:])
+
+
+
+
+
+       
+
+
+
+
+        #do some processing on first, second, and last items
+
+  
+
+
+
+
+
+
+
     print(allPitches)
-    return allPitches
+    return jsonify(splitArray)
