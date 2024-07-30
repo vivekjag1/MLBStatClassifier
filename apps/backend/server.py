a3fc63e07c72c7f6d700b66291c25607c3ed3c72
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-07-30 10:23:04 -0400

    ORM config

--- apps/backend/server.py
+++ apps/backend/server.py
@@ -6,15 +6,49 @@ from myRoute import myRoute
 from routes.exampleRoute import exampleRoute
 from routes.seedDB import seedDatabase
 from configORM import ConfigORM
-api.config.from_object(ConfigORM)   
+api.config['SQLALCHEMY_DATABASE_URI'] = ConfigORM.SQLALCHEMY_DATABASE_URI
+api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(api)
+
+
 api.register_blueprint(myRoute, url_prefix='/api')
 api.register_blueprint(exampleRoute, url_prefix='/api')
 api.register_blueprint(seedDatabase, url_prefix='/api')
-client = SQLAlchemy(api)
 
 
+#tables 
+class Player(db.Model): 
+    __tablename__ = "Player"
+    playerID = db.Column(db.Integer, unique=True, primary_key = True)
+    name = db.Column(db.String, unique = True)
+    handedness = db.Column(db.String)
+    def __repr__(self):
+        return f'<Pitcher {self.name}>'
+class Pitch(db.Model): 
+    __tablename__ = "Pitch"
+    pitcherName = db.Column(db.String(100), db.ForeignKey('Player.name'), primary_key = True)
+    pitchType = db.Column(db.String(100), primary_key=True)
+    releaseSpeed = db.Column(db.Integer)
+    spinRate = db.Column(db.Integer)
+    movementInches = db.Column(db.Integer)
+    alanActiveSpinPct = db.Column(db.Integer)
+    activeSpin = db.Column(db.Integer)
+    hawkeyeMeasured = db.Column(db.Integer)
+    movementInferred = db.Column(db.Integer)
+    def __repr__(self):
+        return f'<Pitch from {self.pitcherName}>'
+    
+@api.route('/initDB')
+def initDB(): 
+    with api.app_context(): 
+        db.create_all()
+    return "Success!"
+
+
+    
 @api.route('/health')
 def healthCheck():
+
     response = {
         "message":"backend healthy!!"
     }
