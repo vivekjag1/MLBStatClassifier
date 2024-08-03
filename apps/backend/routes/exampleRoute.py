dd67159562a6447496e2d8cb3d4af7e3d39232a4
Vivek <vivekjag1@gmail.com>
2024-08-03 14:35:28 -0400

    classification route

--- apps/backend/routes/exampleRoute.py
+++ apps/backend/routes/exampleRoute.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 exampleRoute = Blueprint('exampleRoute', __name__)
 @exampleRoute.route('/exampleRoute', methods=['GET'])
 def handler(): 
+
     res = {
         "message":"hello world!"
     }
--- apps/backend/routes/makeClassification.py
+++ apps/backend/routes/makeClassification.py
@@ -1,6 +1,12 @@
-from flask import Blueprint, request
-
+from flask import Blueprint, request, jsonify
+import joblib
 makeClassification = Blueprint('makeClassification', __name__)
 @makeClassification.route('/makeClassification', methods = ['POST'])
 def handler(): 
-    return request
+    model = joblib.load('./ML/classifier.pkl')
+    req = request.json
+    input = [req['velocity'], req['z-break'], req['rise'], req['x-break'], req['tail']]
+    res =  model.predict([input])
+    retVal =  [int(item) for item in res]
+
+    return ({"result":retVal})
