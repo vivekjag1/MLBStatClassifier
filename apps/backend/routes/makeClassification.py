1473a55ec118005831fdb219615f52d86bd45c8a
Vivek <vivekjag1@gmail.com>
2024-08-03 12:04:19 -0400

    er

--- apps/backend/routes/makeClassification.py
+++ apps/backend/routes/makeClassification.py
@@ -1,4 +1,4 @@
-from flask import Blueprint 
+from flask import Blueprint, request
 
 makeClassification = Blueprint('makeClassification', __name__)
 @makeClassification.route('/makeClassification', methods = ['POST'])
