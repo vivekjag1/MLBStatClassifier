cfc001ca58880c9d5c9c5ba390c597dd3eacb2ee
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-08-03 22:15:50 -0400

    add gradient and transition images

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -33,11 +33,11 @@ export default function Home() {
     }
     return setTimeout(() =>{
       return(
-        <>
+        <motion.div>
               <Image src = {imageArr[nextIndex(imageIndex)]} alt ="fenway hero" layout="fill"/>
-        </>
+        </motion.div>
       )
-    }, 3000)
+    }, 200)
    
 
 
@@ -45,12 +45,33 @@ export default function Home() {
 
 
   }
+  const imageString = `absolute inset-0 w-full h-full object-cover transition-opacity duration-1000 ease-in-out  `
 
   return (
-    <div className = "h-screen w-screen">
-                    <Image src = {imageArr[(imageIndex)]} alt ="fenway hero" layout="fill"/>
+    <div className=" h-screen w-screen">
+      {
+        imageArr.map((image, index) =>{
+          return(
+            <motion.div
+              key = {index}
+              initial = {{opacity:15}}
+              animate = {{opacity: index === imageIndex? 1: 0}}
+              transition={{duration: 1}}
+              className="absolute inset-0"
 
+              >
+                        <div className="absolute flex items-center justify-center inset-0 bg-gradient-to-b from-blue-500 to-blue-900 opacity-40 z-10" />
+
+                                  <Image src={imageArr[index]} alt={`Hero Image ${index}`} layout="fill" className={imageString} />
+
+              </motion.div>
+          )
+        })
+      }
     </div>
+  
+    
+
 
   );
 }
