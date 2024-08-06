e70bafae1d96699dc5b7709319a5e2ce9222ee09
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-08-06 16:38:41 -0400

    add allow pickle

--- apps/backend/routes/getAverages.py
+++ apps/backend/routes/getAverages.py
@@ -3,9 +3,9 @@ import numpy as np
 getAverages = Blueprint('getAverages', __name__)
 @getAverages.route('/getAverages', methods=['GET'])
 def handler(): 
-    testingFeatures = np.load("./numpyArrays/testingFeatures.npy")
+    testingFeatures = np.load("./numpyArrays/testingFeatures.npy", allow_pickle=True)
     trainingFeatures = np.load("./numpyArrays/trainingFeatures.npy")
-    allItems = np.concatenate((trainingFeatures, testingFeatures), axis=0)
+    allItems = np.concatenate((trainingFeatures, testingFeatures), axis=0, allow_pickle=True)
     averages = np.mean(allItems, axis = 0)
     averages = averages.tolist()
 
