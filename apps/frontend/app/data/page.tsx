f25bba2ff9fb97426473d6042b292dc5cf27d9c5
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-08-06 23:15:55 -0400

    add table & deployment fixes

--- apps/frontend/app/data/page.tsx
+++ apps/frontend/app/data/page.tsx
@@ -1,20 +1,36 @@
+"use client"; 
 import { Pitch, columns } from "@/components/customColumns";
 import { CustomTable } from "@/components/customTable";
 import axios from "axios"; 
+import { useEffect, useState } from "react";
 
-const fetchData = async() => {
-    const data = await axios.get('/api/getPitches'); 
-    return data.data; 
-}
 
-export default async function Page(){
-    const data = await fetchData(); 
+
+export default   function Page(){
+    const [pitchData, setPitchData] = useState<Pitch[]>([])
+    const [fetched, setFetched] = useState<boolean>(false); 
+    useEffect(() => {
+        const fetchData = async() => {
+            const data = await axios.get('/api/getPitches'); 
+            setPitchData(data.data)
+            return data.data; 
+        }
+
+        if(!fetched){
+            fetchData().then(); 
+            setFetched(true); 
+        }
+        else{
+            return; 
+        }
+
+    })
     return(
         <>
         <div className = "text-center font-mono text-3xl">
             Pitch Data
             <div className="container mx-auto py-10 font-serif items-center justify-center">
-            <CustomTable columns={columns} data={data} />
+            <CustomTable columns={columns} data={pitchData} />
          </div>    
         </div>
        
