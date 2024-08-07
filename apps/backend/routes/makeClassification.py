2616434ca1cd7056f1b440d53fae6e1d3abb8c82
Vivek Jagadeesh <107503192+vivekjag1@users.noreply.github.com>
2024-08-06 21:53:42 -0400

    add clear function to classifier form

--- apps/backend/routes/makeClassification.py
+++ apps/backend/routes/makeClassification.py
@@ -1,18 +1,12 @@
 from flask import Blueprint, request, jsonify
 import pickle 
 makeClassification = Blueprint('makeClassification', __name__)
-
 with open('./ML/classifier.pkl', "rb") as f: 
         model = pickle.load(f)
-        
 @makeClassification.route('/makeClassification', methods = ['POST'])
 def handler(): 
-
     req = request.json
     input = [req['velocity'], req['z-break'], req['rise'], req['x-break'], req['tail']]
-
-
     res =  model.predict([input])
     retVal =  [int(item) for item in res]
-
     return ({"result":retVal})
--- apps/frontend/components/classifierForm.tsx
+++ apps/frontend/components/classifierForm.tsx
@@ -5,13 +5,21 @@ import axios from "axios"
 import { PitchProvider, usePitchContext } from "@/contexts/pitchContext"
 
 const ClassifierForm = () => {
-    const [velocity, setVelocity] = useState<number>()
-    const [zbreak, setZBreak] = useState<number>(); 
-    const [rise, setRise] = useState<number>(); 
-    const [xbreak, setXBreak] = useState<number>(); 
-    const [tail, setTail] = useState<number>(); 
-    const [pitchType, setPitchType] = useState<number|undefined>(undefined); 
+    const [velocity, setVelocity] = useState<string>()
+    const [zbreak, setZBreak] = useState<string>(); 
+    const [rise, setRise] = useState<string>(); 
+    const [xbreak, setXBreak] = useState<string>(); 
+    const [tail, setTail] = useState<string>(); 
     const {setPitch} = usePitchContext(); 
+    const handleClear = () =>{
+        setVelocity(''); 
+        setZBreak(''); 
+        setRise(''); 
+        setXBreak(''); 
+        setTail(''); 
+
+    
+    }
 
 
 
@@ -44,13 +52,13 @@ const ClassifierForm = () => {
                 justifyContent:"center", 
                 alignItems:"center"
             }}>
-                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch velocity in miles per hour" value={velocity} onChange={(e) => setVelocity(parseFloat(e.target.value))}/>
-                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch break in z-direction in inches" value={zbreak} onChange={(e) => setZBreak(parseFloat(e.target.value))}/>
-                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch rise in inches" value={rise} onChange={(e) => setRise(parseFloat(e.target.value))}/>
-                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch break in the X-direction in inches" value={xbreak} onChange={(e) => setXBreak(parseFloat(e.target.value))}/>
-                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch tail in inches" value={tail} onChange={(e) => setTail(parseFloat(e.target.value))}/>
+                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch velocity in miles per hour" value={velocity} onChange={(e) => setVelocity((e.target.value))}/>
+                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch break in z-direction in inches" value={zbreak} onChange={(e) => setZBreak((e.target.value))}/>
+                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch rise in inches" value={rise} onChange={(e) => setRise((e.target.value))}/>
+                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch break in the X-direction in inches" value={xbreak} onChange={(e) => setXBreak((e.target.value))}/>
+                <Input type = "number" className= "w-3/4 mt-5 " placeholder="Enter the pitch tail in inches" value={tail} onChange={(e) => setTail((e.target.value))}/>
                 <div className="mt-10 flex flex-row px-5">
-                    <Button variant="destructive" className="m-[2rem] w-[7rem]" type="button">Clear </Button>
+                    <Button variant="destructive" className="m-[2rem] w-[7rem]" type="button" onClick={handleClear}>Clear </Button>
                     <Button variant="default" className="m-[2rem] w-[7rem] " type="submit" onClick={handleSubmit}>Submit</Button>
                 </div>
             </form>
